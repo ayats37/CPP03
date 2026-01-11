9685326d83232651bcbedd762e6aa10c33f665ae
ayats37 <ayatouirass27@gmail.com>
2026-01-11 11:06:16 +0100

    dewhfh

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: taya <taya@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 10:36:48 by taya              #+#    #+#             */
-/*   Updated: 2026/01/09 13:18:12 by taya             ###   ########.fr       */
+/*   Updated: 2026/01/11 10:49:30 by taya             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,25 @@
 
 class ClapTrap{
   private :
-    std::string name;
-    unsigned int Hit_points;
-    unsigned int Energy_points;
-    unsigned int Attack_damage;
+    std::string _name;
+    unsigned int _hitPoints;
+    unsigned int _energyPoints;
+    unsigned int _attackDamage;
   public :
     ClapTrap();
     ClapTrap (const std::string& name);
+    ClapTrap(const ClapTrap& other);
+    ClapTrap& operator=(const ClapTrap& other);
     ~ClapTrap();
 
     void attack(const std::string& target);
     void takeDamage(unsigned int amount);
     void beRepaired(unsigned int amount);
+
+    const std::string& getName() const;
+    unsigned int getHitPoints() const;
+    unsigned int getEnergyPoints() const;
+    unsigned int getAttackDamage() const;
 
 };
 
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,24 +6,51 @@
 /*   By: taya <taya@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 10:36:51 by taya              #+#    #+#             */
-/*   Updated: 2026/01/09 13:12:58 by taya             ###   ########.fr       */
+/*   Updated: 2026/01/11 11:05:37 by taya             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
+#include <iostream>
+
+static void printStatus(const ClapTrap& c)
+{
+    std::cout << "[STATUS] " << c.getName() << " | HP: " << c.getHitPoints() << " | EP: " << c.getEnergyPoints() << " | AD: " << c.getAttackDamage() << std::endl;
+}
 
 int main()
 {
-  ClapTrap a("Aya");
+    std::cout << "=== Basic actions ===" << std::endl;
+    ClapTrap a("Aya");
 
-  a.attack("monster");
-  a.takeDamage(5);
-  a.beRepaired(3);
+    printStatus(a);
+    a.attack("Monster");
+    a.takeDamage(3);
+    a.beRepaired(2);
+    printStatus(a);
 
-  a.attack("enemy");
-  a.takeDamage(20);
-  a.attack("ptipana");
-  a.beRepaired(5);
+    std::cout << "\n=== Energy runs out ===" << std::endl;
+    ClapTrap b("EnergyTest");
+    for (int i = 0; i < 12; i++)
+      b.attack("Dummyyyyyy");
+    printStatus(b);
+    b.beRepaired(5);
 
-  return 0;
+    std::cout << "\n=== Hit points go to 0 ===" << std::endl;
+    ClapTrap c("ptipana");
+    c.takeDamage(20);
+    c.attack("Enemy");
+    c.beRepaired(5);
+    printStatus(c);
+
+    std::cout << "\n=== Copy constructor ===" << std::endl;
+    ClapTrap d(a);
+    printStatus(d);
+
+    std::cout << "\n=== Copy assignment ===" << std::endl;
+    ClapTrap e;
+    e = a;
+    printStatus(e);
+
+    return 0;
 }
